7681ebbcfb450ee794e7e9e3f1e39dccbf0722e5
helexander <jeremychee97@gmail.com>
2021-08-11 17:27:15 +1000

    Error and API handling for sign up page, and storing token with async storage

---
+++ src/context/AuthContext.js
@@ -0,0 +1,47 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+
+const authReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload };
+        case 'signup':
+            // Resets error message once user signs up and would proceed to resetting the state
+            return { errorMessage: '', token: action.payload };
+        default:
+            return state;
+    }
+};
+
+const signup = (dispatch) => {
+    return async ({ email, password }) => {
+        try {
+            const response = await trackerApi.post('/signup', { email, password });
+            await AsyncStorage.setItem('token', response.data.token);
+            dispatch({ type: 'signup', payload: response.data.token });
+        } catch (err) {
+            dispatch({ type: 'add_error', payload: 'Something went wrong with signing up' })
+        }
+    }
+}
+
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+
+    }
+}
+
+const signout = (dispatch) => {
+    return () => {
+
+    }
+}
+
+export const { Provider, Context } = createDataContext(
+    authReducer,
+    { signin, signout, signup },
+    // By default the user is not logged in because there is no token
+    // As soon as there is a token present, it would signal that the user is logged in 
+    { token: null, errorMessage: '' }
+);
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer'
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
-    return <>
+    const { state, signup } = useContext(AuthContext);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return <View style={styles.container}>
         <Spacer>
             <Text h3>Sign Up for Tracker</Text>
         </Spacer>
-        <Input label="Email" />
+        <Input label="Email" value={email} onChangeText={(newEmail) => setEmail(newEmail)} autoCapitalize="none" autoCorrect={false} />
         <Spacer />
-        <Input label="Password" />
+        <Input label="Password" value={password} onChangeText={(newPassword) => setPassword(newPassword)} autoCapitalize="none" autoCorrect={false} secureTextEntry={true} />
+        {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
         <Spacer>
-            <Button title="Sign Up" />
+            <Button title="Sign Up" onPress={() => signup({ email, password })} />
         </Spacer>
 
-    </>
+    </View>
 };
 
-const styles = StyleSheet.create({});
+// To hide header section
+SignupScreen.navigationOptions = () => {
+    return {
+        headerShown: false,
+    };
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200
+    },
+    errorMessage: {
+        marginLeft: 10,
+        fontSize: 16,
+        color: 'red'
+    }
+});
 
 export default SignupScreen;
