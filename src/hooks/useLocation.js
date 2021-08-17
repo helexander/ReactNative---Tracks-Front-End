e9934a28cac2004eed6b5500beae2309ba8b6e60
helexander <jeremychee97@gmail.com>
2021-08-17 20:15:54 +1000

    Added feature which disables location tracking when user navigates away from create track screen

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,29 +1,41 @@
 import { useState, useEffect } from "react";
 import { Accuracy, requestForegroundPermissionsAsync, watchPositionAsync } from "expo-location";
 
-export default (callback) => {
+export default (shouldTrack, callback) => {
 
     const [err, setErr] = useState(null);
+    const [subscriber, setSubscriber] = useState(null);
 
     const startWatching = async () => {
         try {
             await requestForegroundPermissionsAsync();
-            await watchPositionAsync({
+
+            const sub = await watchPositionAsync({
                 accuracy: Accuracy.BestForNavigation,
                 timeInterval: 1000,
                 distanceInterval: 10
             },
                 // Gets invoked every single time the user changes its location
                 callback
-            )
+            );
+
+            setSubscriber(sub);
+
         } catch (e) {
             setErr(e);
         }
     }
 
     useEffect(() => {
-        startWatching();
-    }, []);
+        if (shouldTrack) {
+            startWatching();
+        } else {
+            if (subscriber) {
+                subscriber.remove();
+                setSubscriber(null);
+            }
+        }
+    }, [shouldTrack]);
 
     return [err];
 }
