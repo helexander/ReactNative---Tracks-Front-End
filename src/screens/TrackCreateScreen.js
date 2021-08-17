43d1db2b8650989fd825aa7bb5ad27758e3a6b85
helexander <jeremychee97@gmail.com>
2021-08-17 14:00:09 +1000

    Implemented live location update on map

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,12 +1,14 @@
 import '../_mockLocation';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackCreateScreen = () => {
+    const { addLocation } = useContext(LocationContext);
     const [err, setErr] = useState(null);
 
     const startWatching = async () => {
@@ -16,9 +18,11 @@ const TrackCreateScreen = () => {
                 accuracy: Accuracy.BestForNavigation,
                 timeInterval: 1000,
                 distanceInterval: 10
-            }, (location) => {
-                console.log(location);
-            })
+            },
+                // Gets invoked every single time the user changes its location
+                (location) => {
+                    addLocation(location);
+                })
         } catch (e) {
             setErr(e);
         }
