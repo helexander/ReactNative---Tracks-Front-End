aa8ff5c722f83d81458a4d656a469eec4868153d
helexander <jeremychee97@gmail.com>
2021-08-17 15:36:26 +1000

    Extracted logic to useLocation hook

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Text, StyleSheet, ActivityIndicator } from 'react-native';
-import MapView, { Polyline } from 'react-native-maps';
+import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
@@ -16,12 +16,20 @@ const Map = () => {
             latitudeDelta: 0.01,
             longitudeDelta: 0.01,
         }}
-        region={{
-            ...currentLocation.coords,
-            latitudeDelta: 0.01,
-            longitudeDelta: 0.01,
-        }}
+    // Follows user location every time location changes
+    // region={{
+    //     ...currentLocation.coords,
+    //     latitudeDelta: 0.01,
+    //     longitudeDelta: 0.01,
+    // }}
     >
+        <Circle
+            center={currentLocation.coords}
+            radius={30}
+            strokeColor="rgba(158, 158, 255,1.0)"
+            fillColor="rgba(158, 158, 255, 0.03)"
+
+        />
     </MapView>
 }
 
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,36 +1,18 @@
 import '../_mockLocation';
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
-import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
+import useLocation from '../hooks/useLocation';
 
 const TrackCreateScreen = () => {
     const { addLocation } = useContext(LocationContext);
-    const [err, setErr] = useState(null);
 
-    const startWatching = async () => {
-        try {
-            await requestForegroundPermissionsAsync();
-            await watchPositionAsync({
-                accuracy: Accuracy.BestForNavigation,
-                timeInterval: 1000,
-                distanceInterval: 10
-            },
-                // Gets invoked every single time the user changes its location
-                (location) => {
-                    addLocation(location);
-                })
-        } catch (e) {
-            setErr(e);
-        }
-    }
-
-    useEffect(() => {
-        startWatching();
-    }, []);
+    const [err] = useLocation((location) => {
+        addLocation(location)
+    })
 
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
