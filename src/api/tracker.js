c872ef76dbabeae35a9dcc5f9b6e89783a1933df
helexander <jeremychee97@gmail.com>
2021-08-24 13:08:25 +1000

    Added saving tracks and location to database, resetting form and navigate user back to track list screen:

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,7 +1,22 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
-    baseURL: 'http://a43e-203-121-223-250.ngrok.io'
+const instance = axios.create({
+    baseURL: 'http://e6ec-58-84-136-104.ngrok.io'
 });
 
+instance.interceptors.request.use(
+    // Will be called automatically every time a request is made
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
+);
+
+export default instance;
--- src/hooks/useSaveTrack.js
+++ src/hooks/useSaveTrack.js
@@ -1,13 +1,18 @@
 import { useContext } from 'react';
 import { Context as TrackContext } from '../context/TrackContext';
 import { Context as LocationContext } from '../context/LocationContext';
+import { navigate } from '../navigationRef';
 
 export default () => {
     const { createTrack } = useContext(TrackContext);
-    const { state: { locations, name } } = useContext(LocationContext);
+    const { state: { locations, name }, reset } = useContext(LocationContext);
 
-    const saveTrack = () => {
-        createTrack(name, locations);
+    const saveTrack = async () => {
+        await createTrack(name, locations);
+
+        // resetting the form then navigate user to screen with list of tracks recorded
+        reset();
+        navigate('TrackList');
     };
 
     return [saveTrack];
